5653703adda86d84642ad2c7d469f28d512489d3
jinbo0122 <lijinbo0122@gmail.com>
2015-01-05 13:19:00 +0800

    Initial Commit

---
+++ BC ID Photo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BC ID Photo
+//
+//  Created by Albert Lee on 1/5/15.
+//  Copyright (c) 2015 Albert Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BC ID Photo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BC ID Photo
+//
+//  Created by Albert Lee on 1/5/15.
+//  Copyright (c) 2015 Albert Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
